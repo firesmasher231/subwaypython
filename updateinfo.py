777ecdfe3eec7187a30a3489ee467adb1eaf6cdf
firesmasher231 <66473811+firesmasher231@users.noreply.github.com>
2023-10-25 22:10:25 +0100

    login

--- updateinfo.py
+++ updateinfo.py
@@ -2,43 +2,148 @@ import requests
 import json
 import time
 
-with open('accounts-batch1.json', 'r') as json_file:
-    data = json_file.read()
+# with open('accounts-batch1.json', 'r') as json_file:
+#     data = json_file.read()
 
-filename = "accounts.json"
+# filename = "accounts.json"
 
-accountsData = json.loads(data)
+# accountsData = json.loads(data)
 
-accounts = []
+accountsData = [{
+		"email": "l50hqx4ihqz9@txcct.com",
+		"token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJtb2R1bGVDb2RlIjoiU1VCX1NUT1JNQk9STiIsImlzcyI6IlRyYW54YWN0b3IgTHRkIiwidXNlclR5cGUiOiIxIiwiZXhwIjoxNjk3OTg4MDM4LCJ0cmFkZXJJZCI6IjE3NDczNTUzNSIsInVzZXJOYW1lIjoibDUwaHF4NGlocXo5QHR4Y2N0LmNvbSIsImlhdCI6MTY5Nzk4NDQzOCwidXNlcklkIjoiMTc0NzM1NTM1IiwicHJvZ3JhbUlkIjoiNiJ9.ouYgk9SritM3Ot8rBn7OwYVKxNMUfeOD1kR1pFX2sv8",
+		"masterToken": "ZmUxNWIyN2FjNTQ1NGVmODkxY2MwZTBlZmZkMjFkMTQ=",
+		"loyaltyId": "6338450248705219",
+		"qrcode": "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=6338450248705219",
+		"points": 250,
+		"squadCode": "b1iufn",
+		"referralCode": "X-63632KO"
+	}]
 
-for account in accountsData:
+token = accountsData[0].get("token")
+email = accountsData[0].get("email")
+masterToken = accountsData[0].get("masterToken")
+squadCode = accountsData[0].get("squadCode")
+referralCode = accountsData[0].get("referralCode")
 
-    time.sleep(1)
+url = 'https://rewards.subway.co.uk/tx-auth/auth/logon'
+headers = {
+    'Accept-Encoding': 'gzip',
+    'appVersion': '1.8.3',
+    'Connection': 'Keep-Alive',
+    'Content-Length': '52',
+    'Content-Type': 'application/json; charset=UTF-8',
+    'deviceId': '00000000-5e1f-f5f9-ffff-ffffef05ac4a',
+    'Host': 'rewards.subway.co.uk',
+    'latitude': '0',
+    'longitude': '0',
+    'moduleCode': 'SUB_STORMBORN',
+    'platform': 'Android',
+    'User-Agent': 'okhttp/4.9.0'
+}
 
-    print(accountsData.index(account))
+myobj = {
+    "username": email,
+    "password": "Password123!",
+}
 
-    token = accountsData[0].get("token")
-    email = accountsData[0].get("email")
-    masterToken = accountsData[0].get("masterToken")
-    squadCode = accountsData[0].get("squadCode")
-    referralCode = accountsData[0].get("referralCode")
+x = requests.post(url, json=myobj, headers=headers)
 
-    url = 'https://rewards.subway.co.uk/tx-sub/members'
+print("Attempting Account Registration: " + str(x.text))
+
+import re
+
+if 1:
+    # verify email code
+
+    ## Get verification code
+
+    username = email.split("@")[0]
+    domain = email.split("@")[1]
+
+    url = 'https://www.1secmail.com/api/v1/?action=getMessages&login=' + username + '&domain=' + domain
+
+    time.sleep(5)
+
+    while True:
+        response = requests.get(url)
+
+        data = response.json()
+        print(data)
+
+        try:
+            emailid = data[0].get("id")
+            break
+        except:
+            print("No verification code found yet, retrying...")
+            time.sleep(5)
+            continue
+
+    url = 'https://www.1secmail.com/api/v1/?action=readMessage&login=' + username + '&domain=' + domain + '&id=' + str(emailid)
+
+    response = requests.get(url)
+
+    # Extract the code using a regular expression
+    match = re.search(r'Your code: ([A-Z0-9]{5})', response.text)
+
+    
+    code = str(match.group(0)).split(" ")[2]
+    
+    if code:
+        print("Acquired verification code: "+ str(code))
+    else:
+        next()
+
+    ##
+
+
+    url = 'https://rewards.subway.co.uk/tx-sub/registration/verification/' + str(code)
     headers = {
-        'Accept-Encoding': 'gzip',
-        'Authorization': token,
-        'Connection': 'Keep-Alive',
-        'Host': 'rewards.subway.co.uk',
-        'User-Agent': 'okhttp/4.9.0'
+    'accept': 'application/json',
+    'Accept-Encoding': 'gzip',
+    'Connection': 'Keep-Alive',
+    'Content-Length': '0',
+    'Host': 'rewards.subway.co.uk',
+    'moduleCode': 'SUB_STORMBORN',
+    'User-Agent': 'okhttp/4.9.0'
     }
 
-    response = requests.get(url, headers=headers)
+    response = requests.put(url, headers=headers)
 
-    if response.status_code == 200:
-        data = response.json()
-        print(data)  # Print the response JSON
-    else:
-        print('Request failed with status code:', response.status_code, response.text)
+    print("Attempting Email Verification: " + str(response.text))
+
+
+# for account in accountsData:
+
+#     time.sleep(1)
+
+#     print(accountsData.index(account))
+
+#     token = accountsData[0].get("token")
+#     email = accountsData[0].get("email")
+#     masterToken = accountsData[0].get("masterToken")
+#     squadCode = accountsData[0].get("squadCode")
+#     referralCode = accountsData[0].get("referralCode")
+
+#     url = 'https://rewards.subway.co.uk/tx-sub/login'
+#     headers = {
+#         'Accept-Encoding': 'gzip',
+#         'Username': email,
+#         'Password': 'Password123!',
+#         'Authorization': token,
+#         # 'Master-Token': masterToken,
+#         'Connection': 'Keep-Alive',
+#         'Host': 'rewards.subway.co.uk',
+#         'User-Agent': 'okhttp/4.9.0'
+#     }
+
+#     response = requests.get(url, headers=headers)
+
+#     if response.status_code == 200:
+#         data = response.json()
+#         print(data)  # Print the response JSON
+#     else:
+#         print('Request failed with status code:', response.status_code, response.text)
 
     # url = 'https://rewards.subway.co.uk/tx-sub/members'
 
@@ -60,7 +165,7 @@ for account in accountsData:
 
     # accounts.append({"email": email, "token": token, "masterToken": masterToken, "loyaltyId": loyaltyId,"qrcode": qrcode, "points": points, "squadCode": squadCode, "referralCode": referralCode })
 
-with open(filename, "w") as f:
-    f.write(str(accounts))
+# with open(filename, "w") as f:
+#     f.write(str(accounts))
 
-print( "Accounts written to file: " + filename )
+# print( "Accounts written to file: " + filename )
