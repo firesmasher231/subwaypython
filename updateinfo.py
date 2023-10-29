93f57e1dd2c2d2e156919018778b84b089777045
firesmasher231 <asjosh7@gmail.com>
2023-10-29 01:06:45 +0000

    a

--- updateinfo.py
+++ updateinfo.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import time
+import re
 
 # with open('accounts-batch1.json', 'r') as json_file:
 #     data = json_file.read()
@@ -9,51 +10,75 @@ import time
 
 # accountsData = json.loads(data)
 
-accountsData = [{
-		"email": "l50hqx4ihqz9@txcct.com",
-		"token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJtb2R1bGVDb2RlIjoiU1VCX1NUT1JNQk9STiIsImlzcyI6IlRyYW54YWN0b3IgTHRkIiwidXNlclR5cGUiOiIxIiwiZXhwIjoxNjk3OTg4MDM4LCJ0cmFkZXJJZCI6IjE3NDczNTUzNSIsInVzZXJOYW1lIjoibDUwaHF4NGlocXo5QHR4Y2N0LmNvbSIsImlhdCI6MTY5Nzk4NDQzOCwidXNlcklkIjoiMTc0NzM1NTM1IiwicHJvZ3JhbUlkIjoiNiJ9.ouYgk9SritM3Ot8rBn7OwYVKxNMUfeOD1kR1pFX2sv8",
-		"masterToken": "ZmUxNWIyN2FjNTQ1NGVmODkxY2MwZTBlZmZkMjFkMTQ=",
-		"loyaltyId": "6338450248705219",
-		"qrcode": "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=6338450248705219",
-		"points": 250,
-		"squadCode": "b1iufn",
-		"referralCode": "X-63632KO"
-	}]
+null = None
 
-token = accountsData[0].get("token")
-email = accountsData[0].get("email")
-masterToken = accountsData[0].get("masterToken")
-squadCode = accountsData[0].get("squadCode")
-referralCode = accountsData[0].get("referralCode")
 
-url = 'https://rewards.subway.co.uk/tx-auth/auth/logon'
-headers = {
-    'Accept-Encoding': 'gzip',
-    'appVersion': '1.8.3',
-    'Connection': 'Keep-Alive',
-    'Content-Length': '52',
-    'Content-Type': 'application/json; charset=UTF-8',
-    'deviceId': '00000000-5e1f-f5f9-ffff-ffffef05ac4a',
-    'Host': 'rewards.subway.co.uk',
-    'latitude': '0',
-    'longitude': '0',
-    'moduleCode': 'SUB_STORMBORN',
-    'platform': 'Android',
-    'User-Agent': 'okhttp/4.9.0'
-}
+accountsData = [
+  {
+    "email": "fblv2tej@vjuum.com",
+    "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJtb2R1bGVDb2RlIjoiU1VCX1NUT1JNQk9STiIsImlzcyI6IlRyYW54YWN0b3IgTHRkIiwidXNlclR5cGUiOiIxIiwiZXhwIjoxNjk4NTQ2ODcyLCJ0cmFkZXJJZCI6IjE3NDc4MjA0MSIsInVzZXJOYW1lIjoiZmJsdjJ0ZWpAdmp1dW0uY29tIiwiaWF0IjoxNjk4NTM5NjcyLCJ1c2VySWQiOiIxNzQ3ODIwNDEiLCJwcm9ncmFtSWQiOiI2In0.LUR0-nzkB13herkHZ9BA98gGXQ0VZWtSpYEInEYdQ5M",
+    "masterToken": "YjFmZTcxZTkxOTM1NDFkZTlmOWU0YTljMDllZGIxMjY=",
+    "loyaltyId": "6338450225779724",
+    "qrcode": "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=6338450225779724",
+    "points": 250,
+    "squadCode": null,
+    "referralCode": "X-63632KO"
+  },
+  {
+    "email": "5ngqvczn1w1@ezztt.com",
+    "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJtb2R1bGVDb2RlIjoiU1VCX1NUT1JNQk9STiIsImlzcyI6IlRyYW54YWN0b3IgTHRkIiwidXNlclR5cGUiOiIxIiwiZXhwIjoxNjk4NTQ2ODgxLCJ0cmFkZXJJZCI6IjE3NDc4MjA0MiIsInVzZXJOYW1lIjoiNW5ncXZjem4xdzFAZXp6dHQuY29tIiwiaWF0IjoxNjk4NTM5NjgxLCJ1c2VySWQiOiIxNzQ3ODIwNDIiLCJwcm9ncmFtSWQiOiI2In0.ICboeZntw_uMWkdOm2cY_swhZfe7O_gR4yb0bzE3dzU",
+    "masterToken": "OGQxNThkMTE1NDA1NGU1MGE2ODQ4Y2Y3OGUwYmE3NTQ=",
+    "loyaltyId": "6338450261829839",
+    "qrcode": "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=6338450261829839",
+    "points": 250,
+    "squadCode": null,
+    "referralCode": "X-63632KO"
+  },
+  {
+    "email": "l1cskx@txcct.com",
+    "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJtb2R1bGVDb2RlIjoiU1VCX1NUT1JNQk9STiIsImlzcyI6IlRyYW54YWN0b3IgTHRkIiwidXNlclR5cGUiOiIxIiwiZXhwIjoxNjk4NTQ2ODkwLCJ0cmFkZXJJZCI6IjE3NDc4MjA0MyIsInVzZXJOYW1lIjoibDFjc2t4QHR4Y2N0LmNvbSIsImlhdCI6MTY5ODUzOTY5MCwidXNlcklkIjoiMTc0NzgyMDQzIiwicHJvZ3JhbUlkIjoiNiJ9.EGPXxkEKvDk5Sd7867RTyAh2K6BGzCgaKOEmSRcp92k",
+    "masterToken": "NzY1ZTE2MTRhYmRlNGIzMjg4ZGFmMjAzY2JkMjk1NjI=",
+    "loyaltyId": "6338450294859969",
+    "qrcode": "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=6338450294859969",
+    "points": 250,
+    "squadCode": null,
+    "referralCode": "X-63632KO"
+  }]
 
-myobj = {
-    "username": email,
-    "password": "Password123!",
-}
 
-x = requests.post(url, json=myobj, headers=headers)
+for i in range(len(accountsData)):
 
-print("Attempting token refresh: " + str(x.text))
+    token = accountsData[i].get("token")
+    email = accountsData[i].get("email")
+    masterToken = accountsData[i].get("masterToken")
+    squadCode = accountsData[i].get("squadCode")
+    referralCode = accountsData[i].get("referralCode")
 
-import re
+    url = 'https://rewards.subway.co.uk/tx-auth/auth/logon'
+    headers = {
+        'Accept-Encoding': 'gzip',
+        'appVersion': '1.8.3',
+        'Connection': 'Keep-Alive',
+        'Content-Length': '52',
+        'Content-Type': 'application/json; charset=UTF-8',
+        'deviceId': '00000000-5e1f-f5f9-ffff-ffffef05ac4a',
+        'Host': 'rewards.subway.co.uk',
+        'latitude': '0',
+        'longitude': '0',
+        'moduleCode': 'SUB_STORMBORN',
+        'platform': 'Android',
+        'User-Agent': 'okhttp/4.9.0'
+    }
 
-if 1:
+    myobj = {
+        "username": email,
+        "password": "Password123!",
+    }
+
+    x = requests.post(url, json=myobj, headers=headers)
+
+    print("Attempting token refresh: " + str(x.text))
+
     # verify email code
 
     ## Get verification code
@@ -69,7 +94,6 @@ if 1:
         response = requests.get(url)
 
         data = response.json()
-        print(data)
 
         try:
             emailid = data[0].get("id")
@@ -86,11 +110,13 @@ if 1:
     # Extract the code using a regular expression
     match = re.search(r'Your code: ([A-Z0-9]{6})', response.text)
 
-    
-    code = str(match.group(0)).split(" ")[2]
-    
-    if code:
-        print("Acquired verification code: "+ str(code))
+    print("Attempting to extract verification code from email: " + str(response.text))
+    print("Verification code: " + str(match))
+
+    if match:
+        code = str(match.group(0)).split(" ")[2]
+        if code:
+            print("Acquired verification code: "+ str(code))
     else:
         next()
 
@@ -111,6 +137,36 @@ if 1:
     response = requests.put(url, headers=headers)
 
     print("Attempting Email Verification: " + str(response.text))
+
+    token = response.json().get("login").get("token")
+
+    print("Acquired new token: " + str(token))
+
+    url = 'https://rewards.subway.co.uk/tx-sub/members'
+    headers = {
+        "Accept-Encoding": "gzip",
+        "Authorization": token,
+        "Connection": "Keep-Alive",
+        "Host": "rewards.subway.co.uk",
+        "User-Agent": "okhttp/4.9.0"
+    }
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        loyalty_balance = str(data.get("loyaltyBalance"))
+        email = data.get("email")
+        first_name = data.get("firstName")
+        last_name = data.get("lastName")
+
+        # print(response.text.email + " " + response.text.firstName + " " + response.text.lastName + " " + response.text.loyaltyBalance)
+        print(loyalty_balance + " | " + email + " | " + first_name + " | " + last_name) 
+    else:
+        print(f"Request failed with status code: {response.text}")
+
+
 
 
 # for account in accountsData:
